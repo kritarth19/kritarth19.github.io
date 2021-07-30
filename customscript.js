c725582331886211f9644975a0560d704909cc5e
Kritarth-dev <kritarth.business@gmail.com>
2021-07-30 20:09:57 +0530

    mobile changes

--- customscript.js
+++ customscript.js
@@ -3,9 +3,9 @@ const navTextCopy = document.getElementById("nav_big_text")
 var nameCopy = document.getElementById("big_name")
 
 
-if (window.innerWidth < 760) {
-  nameCopy.classList.remove("big_name_active")
-}
+// if (window.innerWidth < 760) {
+//   nameCopy.classList.remove("big_name_active")
+// }
 nameCopy.classList.add("big_name_active")
 
 // Hover effect, big text 
@@ -13,12 +13,14 @@ for (let i=0; i<navLinks.length; i++){
   navLinks[i].addEventListener("mouseover",function(){
     let navLinkText = this.textContent
     navTextCopy.textContent = navLinkText
-    if (window.innerWidth < 760) {
-      navTextCopy.classList.remove("big_text_active")
-    }
-    navTextCopy.classList.add("big_text_active")
+    // if (window.innerWidth < 760) {
+    //   navTextCopy.classList.remove("big_text_active")
+    // }
 
     nameCopy.classList.remove("big_name_active")
+    navTextCopy.classList.add("big_text_active")
+
+    
   })
   
   navLinks[i].addEventListener("mouseout",function(){
@@ -26,9 +28,9 @@ for (let i=0; i<navLinks.length; i++){
     let navLinkText = this.textContent
     navTextCopy.textContent = navLinkText; 
     navTextCopy.classList.remove("big_text_active")
-    if (window.innerWidth < 760) {
-      nameCopy.classList.remove("big_name_active")
-    }
+    // if (window.innerWidth < 760) {
+    //   nameCopy.classList.remove("big_name_active")
+    // }
     nameCopy.classList.add("big_name_active")
 
   })
