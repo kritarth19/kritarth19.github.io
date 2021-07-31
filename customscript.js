d9ebbb0300f1f6e8f4ac373112810d7e8f23e361
Kritarth-dev <kritarth.business@gmail.com>
2021-07-31 18:51:43 +0530

    Basic structure update--base version 1.0 ready

--- customscript.js
+++ customscript.js
@@ -2,21 +2,15 @@ const navLinks = document.querySelectorAll("nav a")
 const navTextCopy = document.getElementById("nav_big_text")
 var nameCopy = document.getElementById("big_name")
 
-
-// if (window.innerWidth < 760) {
-//   nameCopy.classList.remove("big_name_active")
-// }
+// Big text initialised  
 nameCopy.classList.add("big_name_active")
 navTextCopy.classList.remove("big_text_active")
 
-// Hover effect, big text 
+// Big text, w/ w/o hover  
 for (let i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("mouseover", function () {
     let navLinkText = this.textContent
     navTextCopy.textContent = navLinkText
-    // if (window.innerWidth < 760) {
-    //   navTextCopy.classList.remove("big_text_active")
-    // }
 
     nameCopy.classList.remove("big_name_active")
     navTextCopy.classList.add("big_text_active")
